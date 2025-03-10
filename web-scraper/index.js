b98063722858ddfbdcd6d546e694c3a0733d0346
Shalin-Shah-2002 <2002shalin@gmail.com>
2025-03-10 20:12:43 +0530

    first commit

---
+++ web-scraper/index.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const express = require('express');
+const cheerio = require('cheerio');
+
+const url = "https://fakestoreapi.com/";
+
+async function scraping() {
+    try {
+        // Step:1 It fetches HTML content from website using axios
+        const { data } = await axios.get(url);
+        // Step:2 It loads HTML content in cheerio
+        const $ = cheerio.load(data);
+
+        // Step:3 It selects the element from HTML content
+        const title = [];
+        $('h3').each((i, element) => { title.push($(element).text()) });
+        console.log(title);
+    }
+
+
+    catch (error) {
+        console.error(`Error fetching data: ${error.message}`);
+    }
+}
+
+scraping();
